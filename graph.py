ba860abcd277334cb22a5c7a2625f6bc4138b857
aziz-zzz20 <58660624+aziz-zzz20@users.noreply.github.com>
2022-03-18 20:23:36 +0000

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from math import inf
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class graph:
     """
@@ -9,6 +10,7 @@ class graph:
     def __init__(self):
         self.adj_matrix = None
         self.adj_list = None
+        self.edges = None
         self.num_vertices = 0
         self.num_edges = 0
         self.colors = None
@@ -24,9 +26,11 @@ class graph:
                 [0 for _ in range(self.num_vertices)] for _ in range(self.num_vertices)
             ]
             self.adj_list = [[] for _ in range(self.num_vertices)]
+            self.edges = []
             self.colors = [-1 for _ in range(self.num_vertices)]
 
             if mode == "--adjlist":
+                i = 0
                 adj_list = input[1:]
                 for lst in adj_list:
                     vertex, neighbors = int(lst.split()[0]), [
@@ -37,9 +41,11 @@ class graph:
                     # introducing a new row in the adjacency list
                     self.adj_list[vertex] = neighbors
 
-                    # introducing a new row in the adjacency matrix
+                    # introducing a new row in the adjacency matrix and updating edges list
                     for neighbor in neighbors:
                         self.adj_matrix[vertex][neighbor] = 1
+                        if (neighbor,vertex) not in self.edges:
+                            self.edges.append((vertex,neighbor))
             else:
                 edges = input[1:]
                 assert len(edges) == self.num_edges
@@ -49,7 +55,8 @@ class graph:
                     assert len(edge) == 2
                     assert 0 <= edge[0] and edge[0] < self.num_vertices
                     assert 0 <= edge[0] and edge[0] < self.num_vertices
-
+                    
+                    
                     # append each vertex of the edge as neighbor to the other
                     self.adj_list[edge[0]].append(edge[1])
                     self.adj_list[edge[1]].append(edge[0])
@@ -57,6 +64,9 @@ class graph:
                     # set the neighborhood in the adjacency matrix
                     self.adj_matrix[edge[0]][edge[1]] = 1
                     self.adj_matrix[edge[1]][edge[0]] = 1
+                    
+                    # update edges list
+                    self.edges.append((edge[0],edge[1]))
 
     def get_neighbors(self, vertex):
         assert vertex < self.num_vertices
@@ -66,3 +76,9 @@ class graph:
         neighbors = self.get_neighbors(vertex)
         neighbors_colors = [sub_coloring[i] for i in neighbors if sub_coloring[i] != -1]
         return neighbors_colors
+    
+    def visualize_graph(self):
+        G = nx.Graph()
+        G.add_edges_from(self.edges)
+        nx.draw_networkx(G,node_color=self.colors)
+        plt.show()
