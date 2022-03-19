3bd59f3bcf43ffe16cc911b2e55449fb89fb1be5
ISLAM BENAMIROUCHE <ii_benamirouche@esi.dz>
2022-03-19 02:53:18 +0100

    list containing our B&B Tree elements

--- exact_methods/branch_and_bound.py
+++ exact_methods/branch_and_bound.py
@@ -1,8 +1,10 @@
 import time
+from treelib import Node, Tree
+
 
 
 def branch_and_bound(g):
-    def search(g, sub_coloring, num_colors, num_non_colored):
+    def search(g, sub_coloring, num_colors, num_non_colored,id,coloring_tree):
         if num_non_colored == 0 and num_colors < g.approximative_optimum:
             g.approximative_optimum = num_colors
             g.colors = sub_coloring.copy()
@@ -13,6 +15,9 @@ def branch_and_bound(g):
 
         new_color = num_colors
         extended_sub_coloring = []
+
+        ##########
+        coloring_tree.append((id,sub_coloring))
 
         for i in range(len(sub_coloring)):
             not_yet_colored = num_non_colored
@@ -29,17 +34,26 @@ def branch_and_bound(g):
                 if new_color not in neighbors_colors:
                     possibility[j] = new_color
                     not_yet_colored -= 1
+            
+            possibility_id = f'{id}.{i}'
 
             if (not_yet_colored, possibility) not in extended_sub_coloring:
-                extended_sub_coloring.append((not_yet_colored, possibility))
+                extended_sub_coloring.append((not_yet_colored, possibility,possibility_id))
 
         extended_sub_coloring.sort()
         for extended in extended_sub_coloring:
-            search(g, extended[1], new_color + 1, extended[0])
+            search(g, extended[1], new_color + 1, extended[0],extended[2],coloring_tree)
+    
+    def display_tree(coloring_tree):
+        coloring_tree.sort()
+        tree = Tree()
+        tree.create_node('n1','n1')
+        for element in coloring_tree:
+            print(f'Id={element[0]} : {element[1]}')    
 
     sub_coloring = [-1 for _ in range(g.num_vertices)]
-    search(g, sub_coloring, 0, g.num_vertices)
-
+    coloring_tree=[]
+    search(g, sub_coloring, 0, g.num_vertices,str(0),coloring_tree)
 
 def measure_execution_time(g):
     print(f"Number of vertices: {g.num_vertices}, number of edges: {g.num_edges}")
@@ -49,3 +63,5 @@ def measure_execution_time(g):
     print("optimum number of colors: ", g.approximative_optimum)
     print("coloring: ", g.colors)
     print("Execution time: ", end_time - start_time)
+
+
