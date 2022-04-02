f414965c3bcdeece4eb9cf9328a0ff80cd4e06a0
aziz-zzz20 <ga_hamdi@esi.dz>
2022-04-02 01:33:56 +0100

    commenting main.py code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
 from graph import graph
-from exact_methods.dynamic_programming import measure_execution_time
+from exact_methods.branch_and_bound import measured_branch_and_bound
 
 if __name__ == "__main__":
+    # Check the validity of the usage.
     if len(sys.argv) != 2:
         print("error: wrong number of arguements.")
         print("usage: python3 main.py input_file.txt\n")
@@ -11,5 +12,9 @@ if __name__ == "__main__":
 
     input_file = sys.argv[1]
 
+    # Create a graph from the input file.
     g = graph()
     g.read(input_file)
+
+    # Color the graph g and mesure the time it takes.
+    measured_branch_and_bound(g, False, True)
