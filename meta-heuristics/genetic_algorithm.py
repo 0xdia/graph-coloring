0381bc38ce1f309e96c0e93e3d1b908fdc0fe4f3
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2022-05-17 11:27:36 +0100

    implement k-points crossing

--- meta-heuristics/genetic_algorithm.py
+++ meta-heuristics/genetic_algorithm.py
@@ -1,7 +1,8 @@
 from random import randint, seed
 from time import time
 
-seed(time()) # initialize the seed
+seed(time())  # initialize the seed
+
 
 def crossing(father, mother, manner="1point"):
     def uniform_crossing(father, mother):
@@ -11,11 +12,36 @@ def crossing(father, mother, manner="1point"):
                 first_child[i], second_child[i] = second_child[i], first_child[i]
         return first_child, second_child
 
+    def k_points_crossing(father, mother, k):
+        # to avoid infinite loop when looking for random points
+        assert k <= len(father) - 1
+
+        segments = [0, len(father)]
+        while len(segments) < k + 2:
+            random_point = randint(1, len(father) - 1)
+            if random_point not in segments:
+                segments.append(random_point)
+        segments.sort()
+
+        first_child, second_child = father.copy(), mother.copy()
+        cross = False
+        for i in range(len(segments) - 1):
+            if cross:
+                (
+                    first_child[segments[i] : segments[i + 1]],
+                    second_child[segments[i] : segments[i + 1]],
+                ) = (
+                    second_child[segments[i] : segments[i + 1]],
+                    first_child[segments[i] : segments[i + 1]],
+                )
+            cross = not cross
+        return first_child, second_child
+
     assert len(father) == len(mother)
     return (
         uniform_crossing(father, mother)
         if manner == "uniforme"
-        else None  # k_points_croisement(father, mother, int(manner[0]))
+        else k_points_crossing(father, mother, int(manner))
     )
     pass
 
@@ -23,7 +49,7 @@ def crossing(father, mother, manner="1point"):
 father = [1, 2, 3, 4, 5, 6]
 mother = [10, 20, 30, 40, 50, 60]
 
-first, second = crossing(father, mother, "uniforme")
+first, second = crossing(father, mother, "4")
 
 print(father)
 print(mother)
