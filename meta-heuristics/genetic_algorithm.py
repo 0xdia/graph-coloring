3ea55237e04059f3d0bb1bb6dbe3ee0e0f9d4fa1
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2022-05-17 14:54:43 +0100

    implement roulette selection

--- meta-heuristics/genetic_algorithm.py
+++ meta-heuristics/genetic_algorithm.py
@@ -1,7 +1,58 @@
-from random import randint, seed
+from random import randint, seed, uniform
 from time import time
 
 seed(time())  # initialize the seed
+
+
+def selection(population, strategy="random", percentage=0.5):
+    assert 0.0 < percentage and percentage < 1.0 
+    pool = []
+
+    def roulette():
+        unique_colors = [len(set(individual)) for individual in population]
+        n = sum(unique_colors)
+        pop_size = len(population)
+        sectors = [
+            ((n - num_colors) * 360 / float(n * (pop_size - 1)))
+            for num_colors in unique_colors
+        ]
+
+        for i in range(1, len(sectors)):
+            sectors[i] += sectors[i - 1]
+
+        selected = [False for _ in range(len(population))]
+        while len(pool) < int(pop_size * percentage):
+            random_deg = uniform(0.0, 359.99)
+            for i in range(0, len(sectors)):
+                if random_deg <= sectors[i]:
+                    if selected[i]:
+                        break
+                    pool.append(population[i])
+                    selected[i] = True
+                    break
+
+    def elitist():
+        pass
+
+    def tournament():
+        pass
+
+    def ranking():
+        pass
+
+    def random():
+        pass
+
+    # this could be replaced with match-case introduced in python 3.10
+    {
+        "roulette": roulette(),
+        "elitist": elitist(),
+        "tournament": tournament(),
+        "ranking": ranking(),
+        "random": random(),
+    }[strategy]
+
+    return pool
 
 
 def crossing(father, mother, manner="1"):
@@ -55,9 +106,9 @@ def mutation(individual):
     individual[gene] = colors[new_gene]
 
 
-father = [1, 2, 3, 4, 5, 5]
-mother = [10, 20, 30, 40, 50, 60]
+a = [1, 2, 3, 4, 5, 5]
+b = [10, 20, 30, 40, 50, 60]
+c = [0, 4, 5, 6, 0, 0]
+d = [2, 5, 6, 7, 9, 7]
 
-mutation(father)
-
-print(father)
+print(selection([a, b, c, d], "roulette"))
