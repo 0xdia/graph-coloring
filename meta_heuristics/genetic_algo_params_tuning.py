1eb8360b70a39712e76409f96c9ddccbc831a1c1
unknown <hw_kribaa@esi.dz>
2022-06-06 13:28:54 +0100

    mutation_proba_impact

--- meta_heuristics/genetic_algo_params_tuning.py
+++ meta_heuristics/genetic_algo_params_tuning.py
@@ -73,15 +73,27 @@ def crossing_probability_impact(g, file_name):
 
 
 
-def mutation_probability_impact(g):
-    new_list = range(math.floor(0), math.ceil(g.num_vertices + 1))
-    plt.yticks(new_list)
+def mutation_probability_impact(g, file_name):
+    figure, axis = plt.subplots(1, 2)
+    axis[0].set_title("Nombres des couleurs optimums")
+    axis[0].legend(loc="best")
+    axis[0].set_xlabel("Iterations")
+    axis[0].set_ylabel("Optimal number of colors")
+    axis[1].set_title("Temps d'execution")
+    axis[1].legend(loc="best")
+    axis[1].set_xlabel("Crossing probabilities")
+    axis[1].set_ylabel("Temps d'execution")
 
     num_iterations = 50
-    probabilities = [0.0, 0.2, 0.5, 0.8, 1.0]
+    probabilities = [0.2, 0.4, 0.6, 0.8, 1.0]
     iterations = list(range(num_iterations))
-
+    
+    times = []
+    optimums = []
+    min_optimum, max_optimum = g.num_vertices, 0
     for proba in probabilities:
+        print("proba = ", proba)
+        start = time()
         result = genetic_algorithm(
             g,
             pool_size=200,
@@ -90,32 +102,69 @@ def mutation_probability_impact(g):
             crossing_proba=0.6,
             crossing_manner="1",
             mutation_proba=proba,
-            nbr_iterations=50,
+            nbr_iterations=40,
             param_tuning=True,
         )
-
+        times.append(time() - start)
+        optimums.append(result[-1])
         if len(result) < num_iterations:
             result.extend([result[-1]] * (num_iterations - len(result)))
-        plt.plot(iterations, result, label=str(proba))
+        min_optimum = min(min_optimum, min(result))
+        max_optimum = max(max_optimum, max(result))
+        axis[0].plot(iterations, result, label=str(proba))
 
         # re-init the the graph
         g.re_initialize_graph()
+        
+    with open(f"D:\graph-coloring\\benchmark\genetic_algorithm\{file_name}\mutation_probability_impact.txt", 'w+') as f:
+        f.write(f"probabilities =  {probabilities}\r\n")
+        f.write(f"times =  {times}\r\n")
+        f.write(f"optimums =  {optimums}\r\n")
+        f.close()
+    print("times = ", times)
+    print("iterations = ", iterations)
+    print("optimums = ", result)
+    axis[0].set_yticks(range(math.floor(0), math.ceil(max(times))))
+    axis[0].set_yticks(range(math.floor(min_optimum), math.ceil(max_optimum)))
+    axis[0].set_ylim(bottom = min_optimum - 1, top = max_optimum)
+    axis[1].plot(probabilities, times)
+    
+    
+    axis[0].legend()
+    axis[1].legend()
+    #plt.savefig(
+    #    f"D:\graph-coloring\\benchmark\genetic_algorithm\{file_name}\mutation_probability_impact.png", bbox_inches="tight"
+    #)
+    #plt.show()
 
-    plt.ylabel("optimal number of colors")
-    plt.title("Impact of the mutation probability")
-    plt.legend(loc="best")
-    plt.savefig("./benchmark/mutation_probability_impact.png", bbox_inches="tight")
-    plt.show()
 
-
-def num_generations_impact(g):
-    new_list = range(math.floor(0), math.ceil(g.num_vertices + 1))
-    plt.yticks(new_list)
+def num_generations_impact(g, file_name):
+    figure, axis = plt.subplots(1, 2)
+    axis[0].set_title("Nombres des couleurs optimums")
+    axis[0].legend(loc="best")
+    axis[0].set_xlabel("Iterations")
+    axis[0].set_ylabel("Optimal number of colors")
+    axis[1].set_title("Temps d'execution")
+    axis[1].legend(loc="best")
+    axis[1].set_xlabel("Crossing probabilities")
+    axis[1].set_ylabel("Temps d'execution")
+    
+    #new_list = range(math.floor(0), math.ceil(g.num_vertices + 1))
+    #plt.yticks(new_list)
+    
+    num_iterations = 50
+    probabilities = [0.2, 0.4, 0.6, 0.8, 1.0]
+    iterations = list(range(num_iterations))
 
     num_generations = [_ for _ in range(20, 101, 20)]
-    iterations = list(range(num_generations[-1]))
-
+    #iterations = list(range(num_generations[-1]))
+    
+    times = []
+    optimums = []
+    min_optimum, max_optimum = g.num_vertices, 0
     for num in num_generations:
+        print("num_generation = ", num)
+        start = time()
         result = genetic_algorithm(
             g,
             pool_size=200,
@@ -127,19 +176,42 @@ def num_generations_impact(g):
             nbr_iterations=num,
             param_tuning=True,
         )
+        
+        times.append(time() - start)
+        optimums.append(result[-1])
+        if len(result) < num_iterations:
+            result.extend([result[-1]] * (num_iterations - len(result)))
+        min_optimum = min(min_optimum, min(result))
+        max_optimum = max(max_optimum, max(result))
+        axis[0].plot(iterations, result, label=str(num))
 
-        if len(result) < num_generations[-1]:
-            result.extend([result[-1]] * (num_generations[-1] - len(result)))
-        plt.plot(iterations, result, label=str(num))
+#        if len(result) < num_generations[-1]:
+#            result.extend([result[-1]] * (num_generations[-1] - len(result)))
+#        plt.plot(iterations, result, label=str(num))
 
         # re-init the the graph
         g.re_initialize_graph()
-
-    plt.ylabel("optimal number of colors")
-    plt.title("Impact of the number of generations (iterations)")
-    plt.legend(loc="best")
-    plt.savefig("./benchmark/num_generations_impact.png", bbox_inches="tight")
+    
+    with open(f"D:\graph-coloring\\benchmark\genetic_algorithm\{file_name}\num_generations_impact.txt", 'w+') as f:
+        f.write(f"probabilities =  {num_generations}\r\n")
+        f.write(f"times =  {times}\r\n")
+        f.write(f"optimums =  {optimums}\r\n")
+        f.close()
+    print("times = ", times)
+    print("iterations = ", iterations)
+    print("optimums = ", result)
+    axis[0].set_yticks(range(math.floor(0), math.ceil(max(times))))
+    axis[0].set_yticks(range(math.floor(min_optimum), math.ceil(max_optimum)))
+    axis[0].set_ylim(bottom = min_optimum - 1, top = max_optimum)
+    axis[1].plot(num_generations, times)
+    
+    axis[0].legend()
+    axis[1].legend()
+    plt.savefig(
+        f"D:\graph-coloring\\benchmark\genetic_algorithm\{file_name}\num_generations_impact.png", bbox_inches="tight"
+    )
     plt.show()
+
 
 
 def pool_size_impact(g):
@@ -149,7 +221,7 @@ def pool_size_impact(g):
     nbr_iterations = 45
     iterations = [_ for _ in range(nbr_iterations)]
 
-    pool_sizes = [size for size in range(100, 701, 300)]
+    pool_sizes = [size for size in range(100, 301, 100)]
     for pool_size in pool_sizes:
         result = genetic_algorithm(
             g,
