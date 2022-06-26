228638d171cc5e61196c6c99f6b0996f3280deca
islamben <islambenamirouche2001@gmail.com>
2022-06-26 12:59:08 +0100

    add grey wolf and ag interfaces

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import sys
 from PyQt5 import QtWidgets
-from screens.bbScreen import Ui_bbScreen
-from screens.tabu import Ui_tabuScreen
-from screens.rlf import Ui_rlfScreen
-from screens.dsatur import Ui_dsaturScreen
-from screens.benchmark import Ui_benchmarkScreen
+
 from screens.home import Ui_MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+    MainWindow.setFixedHeight(600)
+    MainWindow.setFixedWidth(800)
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
